bf81adbbff6a11999f824a6eff1110688761a8a3
Hyacinth22 <you@hijomanta@yahoo.com>
2022-10-13 21:13:18 -0700

    4 commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
-#include <Stdio.h>
+#include <stdio.h>
 /**
- * main-function puts
+ * main-Entry point
  *
- * return: Always 0 (success)
+ * return:Always 0 (successful)
  */
-int main (void)
-{
-	puts("\"Programming is like building a multilingual puzzle");
-		return (0);
+int main(void)
+{puts("\"programming is like building a multilingual puzzle");
+return (0);
 }
